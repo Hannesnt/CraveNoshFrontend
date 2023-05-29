555de2b3f469f6e653db973eab6a170580e250df
Hannesnt <hannesnt@gmail.com>
2023-05-29 14:32:14 +0200

    css fix

--- src/pages/SingleRecipe/SingleRecipe.js
+++ src/pages/SingleRecipe/SingleRecipe.js
@@ -233,7 +233,7 @@ function SingleRecipe({ saveToLocalStorage }) {
       <RecipeKey />
       <div className="row gx-0">
         <div className="col-12 d-flex mb-5 flex-column flex-lg-row">
-          <div className="col-12 col-lg-3 d-flex text-center align-items-lg-center justify-content-center mb-4 mb-lg-0">
+          <div className="col-12 col-lg-3 d-flex text-center align-items-lg-center justify-content-center mb-5 mb-lg-0">
             <h4>LIKNANDE RECEPT:</h4>
           </div>
           <div className="col-12 col-lg-9 d-flex justify-content-center justify-content-lg-between">
